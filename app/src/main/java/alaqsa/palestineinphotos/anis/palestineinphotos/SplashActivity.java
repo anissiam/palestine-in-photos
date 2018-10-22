afad2fd5d36b5a39cea3df352a4357656ffb8d99
anissiam <anis.siam@hotmail.com>
2018-10-22 19:50:58 +0300

    //splash and login in and sign up ready to code

--- app/src/main/java/alaqsa/palestineinphotos/anis/palestineinphotos/SplashActivity.java
+++ app/src/main/java/alaqsa/palestineinphotos/anis/palestineinphotos/SplashActivity.java
@@ -1,7 +1,9 @@
 package alaqsa.palestineinphotos.anis.palestineinphotos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 //
+
+    }
+
+    public void go(View view) {
+        Intent intent = new Intent(this,RegistrationActivity.class);
+        startActivity(intent);
     }
 }
